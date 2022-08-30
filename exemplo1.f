ba5fa3932b32fae188594d865e1d3ebb0d167ddd
Roberto Silva <90939196+Robertobappe@users.noreply.github.com>
2022-08-30 16:31:19 -0300

    Add files via upload

---
+++ exemplo1.f
@@ -0,0 +1,9 @@
+C       programa.f
+        real*8 a3 !variaveis de precisao dupla
+        integer*8 ia1 !interios de precisao dupla
+C       final da declaracao de variaveis
+        a1 = 10
+        a3 = a1*5.5
+        write(*,*)'a1 =',a1 !escreve o resultado de a1
+        write(*,*)'a3 =',a3 !escreve o resultado de a3
+        end !sempre o programa tem que terminar com end (fim dos comandos)
